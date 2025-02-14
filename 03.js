439506ecdcc3ad60514e851f0ea6ebf01c030362
Meet Bhuva <meetbhuva41@gmail.com>
2025-02-14 22:13:37 +0530

    A

--- 03.js
+++ 03.js
@@ -1,12 +1,12 @@
 let score=true
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueInNumber=Number(score)
 
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
 
 
@@ -14,6 +14,21 @@ let isLoggedIn=1
 
 let booleanIsLoggedIn=Boolean(isLoggedIn)
 
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 
+let someNumber=23
+
+let stringNumber=String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+// OPERATIONS //
+
+let value=3
+
+let negValue=-value
+
+console.log(negValue);
