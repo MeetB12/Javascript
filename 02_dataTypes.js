d6344029e7785c157d88a20de70b681fc0abf3c1
Meet Bhuva <meetbhuva41@gmail.com>
2025-02-14 21:56:36 +0530

    03

--- 02_dataTypes.js
+++ 02_dataTypes.js
@@ -3,3 +3,11 @@
 //alert()//we are using nodjs not browser
 
 console.log(3+3)
+
+let age=50
+
+let name="meet"
+
+console.log(typeof age)
+
+console.log(typeof Symbol)
---
+++ 03.js
@@ -0,0 +1,19 @@
+let score=true
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber=Number(score)
+
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+
+
+let isLoggedIn=1
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn)
+
+
