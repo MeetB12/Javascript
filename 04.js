8a761a83995419185676168c183def8a74e70532
Meet Bhuva <meetbhuva41@gmail.com>
2025-02-17 21:47:33 +0530

    B

---
+++ 04.js
@@ -0,0 +1,19 @@
+const name="meet"
+let repocount = 50
+
+console.log(name + repocount)
+
+
+console.log(`hello my name is ${name} `);
+
+
+const gameName=new String("meet")
+
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+
+
---
+++ 05.js
@@ -0,0 +1,32 @@
+const score =400
+// console.log(score);
+
+
+const balance =new Number(100)
+// console.log(balance);
+
+
+
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(2));
+
+
+let otherNumber=123.495244
+
+// console.log(otherNumber.toPrecision(5));
+
+let hundreds=100000000
+// console.log(hundreds.toLocaleString());
+
+
+let mydate=new Date()
+
+console.log(mydate.toString());
+console.log(mydate.toDateString());
+console.log(mydate.toLocaleString());
+console.log(typeof mydate);
+
+
+const myCreateDate=new Date(2023,0,31)
+console.log(myCreateDate.toString());
